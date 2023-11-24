9a10b27ac30b5103f9ed9a65d256218bf3b77bf1
Kacprucha <karas.kacper@gmail.com>
2023-11-24 19:58:18 +0100

    Making Program public

--- P05Shop.API/Program.cs
+++ P05Shop.API/Program.cs
@@ -7,47 +7,54 @@ using P06Shop.API.Services.PersonService;
 using P06Shop.Shared.Services.CarService;
 using P06Shop.Shared.Services.ProductService;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<DataBaseContext>(options => options.UseNpgsql("Server=localhost;Username=postgres;Database=postgres"));
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<ICarService, CarService>();
-builder.Services.AddScoped<ICarBrandService, CarBrandService>();
-builder.Services.AddScoped<IPersonService, PersonService>();
-
-// addScoped - obiekt jest tworzony za kazdym razem dla nowego zapytania http
-// jedno zaptranie tworzy jeden obiekt 
-
-// addTransinet obiekt jest tworzony za kazdym razem kiedy odwolujmey sie do konstuktora 
-// nawet wielokrotnie w cyklu jedengo zaptrania 
-
- //addsingleton - nowa instancja klasy tworzona jest tylko 1 na caly cykl trwania naszej aplikacji 
-
-
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main (string[] args)
+    {
+
+        var builder = WebApplication.CreateBuilder (args);
+
+        builder.Services.AddDbContext<DataBaseContext> (options => options.UseNpgsql ("Server=localhost;Username=postgres;Database=postgres"));
+
+        // Add services to the container.
+
+        builder.Services.AddControllers ();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer ();
+        builder.Services.AddSwaggerGen ();
+
+
+        builder.Services.AddScoped<IProductService, ProductService> ();
+        builder.Services.AddScoped<ICarService, CarService> ();
+        builder.Services.AddScoped<ICarBrandService, CarBrandService> ();
+        builder.Services.AddScoped<IPersonService, PersonService> ();
+
+        // addScoped - obiekt jest tworzony za kazdym razem dla nowego zapytania http
+        // jedno zaptranie tworzy jeden obiekt 
+
+        // addTransinet obiekt jest tworzony za kazdym razem kiedy odwolujmey sie do konstuktora 
+        // nawet wielokrotnie w cyklu jedengo zaptrania 
+
+        //addsingleton - nowa instancja klasy tworzona jest tylko 1 na caly cykl trwania naszej aplikacji 
+
+
+
+
+        var app = builder.Build ();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment ())
+        {
+            app.UseSwagger ();
+            app.UseSwaggerUI ();
+        }
+
+        app.UseHttpsRedirection ();
+
+        app.UseAuthorization ();
+
+        app.MapControllers ();
+
+        app.Run ();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
