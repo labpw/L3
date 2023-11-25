8f46456146c188ec182ba6b1911e2733afb85402
Kacprucha <karas.kacper@gmail.com>
2023-11-25 16:41:43 +0100

    Car tests

--- BackendTestsProject/CarTests.cs
+++ BackendTestsProject/CarTests.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using Moq;
+using P06Shop.Shared.Cars;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BackendTestsProject
 {
-    public class CarTest
+    public class CarTest : IDisposable
     {
-        [Fact]
-        public void postCar_ProperData ()
+        private CustomWebApplicationFactory _factory;
+        private HttpClient _client;
+
+        public CarTest ()
         {
-            
+            _factory = new CustomWebApplicationFactory ();
+            _client = _factory.CreateClient ();
+        }
+
+        [Fact]
+        public async void postCar_ProperData ()
+        {
+            var newCar = new Car ("cos", 2);
+
+            _factory.CarRepositoryMock.Setup(r => r.AddCar (It.Is<Car>(c => c.Model == "cos" && c.Power == 2))).Verifiable ();
+
+            var response = await _client.PostAsync ("api/Car", JsonContent.Create (newCar));
+
+            Assert.Equal (HttpStatusCode.Created, response.StatusCode);
+
+            _factory.CarRepositoryMock.VerifyAll ();
         }
 
         [Fact]
@@ -60,6 +81,12 @@ namespace BackendTestsProject
         public void deleteCar_WrongId ()
         {
             
+        }
+
+        public void Dispose ()
+        {
+            _client.Dispose ();
+            _factory.Dispose ();
         }
     }
 }
--- BackendTestsProject/CustomWebApplicationFactory.cs
+++ BackendTestsProject/CustomWebApplicationFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using P05Shop.API.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,22 @@ namespace BackendTestsProject
 {
     public class CustomWebApplicationFactory: WebApplicationFactory<Program>
     {
+        public Mock<ICarRepository> CarRepositoryMock { get; }
+        //public Mock
+
+        public CustomWebApplicationFactory ()
+        {
+            CarRepositoryMock = new Mock<ICarRepository>();
+        }
+
         protected override void ConfigureWebHost (IWebHostBuilder builder)
         {
             base.ConfigureWebHost (builder);
 
-            
+            builder.ConfigureTestServices (services =>
+            {
+                services.AddSingleton (CarRepositoryMock.Object);
+            });
         }
     }
 }
